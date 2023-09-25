87e827af266331167b331cf708fd29a45cfd9a29
Pawan Kushwah <pawankushwahmail@gmail.com>
2023-09-25 11:28:31 +0530

    decreased the size of image in the card

--- app/(main)/imageSlider.tsx
+++ app/(main)/imageSlider.tsx
@@ -23,7 +23,7 @@ export default function ImageSlider({ images, imagePath }: propsType) {
   return (
     <div className="flex relative overflow-x-auto select-none h-fit">
       <div className="w-full h-40 flex justify-center items-center">
-      <Image src={`${imagePath}/${images[slide-1]}`} className="h-full" alt="slide" width={400} height={400} />
+      <Image src={`${imagePath}/${images[slide-1]}`} className="h-full" alt="slide" width={200} height={200} />
       </div>
       <div className="absolute top-1/2 -translate-y-1/2 left-5 right-5 flex justify-between items-center">
         <Link href={"#"} onClick={handlePrev} id="prev" className="bg-gray-800 hover:bg-gray-500 rounded-full cursor-pointer p-5 w-5 h-5 flex justify-center items-center opacity-50 hover:opacity-100">❮</Link>
