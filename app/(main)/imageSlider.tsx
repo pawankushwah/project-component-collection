5840a36080e2f36c3fba252a5e93030e18e19493
Pawan Kushwah <pawankushwahmail@gmail.com>
2023-09-25 11:20:05 +0530

    added searchTypes (product or component) with some data of both

--- app/(main)/imageSlider.tsx
+++ app/(main)/imageSlider.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 
 type propsType = {
   images: String[];
+  imagePath: String;
 };
 
-export default function ImageSlider({ images }: propsType) {
+export default function ImageSlider({ images, imagePath }: propsType) {
   const [slide, setSlide] = useState(1);
 
   function handleNext(){
@@ -22,7 +23,7 @@ export default function ImageSlider({ images }: propsType) {
   return (
     <div className="flex relative overflow-x-auto select-none h-fit">
       <div className="w-full h-40 flex justify-center items-center">
-      <Image src={`/images/${images[slide-1]}`} className="h-full" alt="slide" width={400} height={400} />
+      <Image src={`${imagePath}/${images[slide-1]}`} className="h-full" alt="slide" width={400} height={400} />
       </div>
       <div className="absolute top-1/2 -translate-y-1/2 left-5 right-5 flex justify-between items-center">
         <Link href={"#"} onClick={handlePrev} id="prev" className="bg-gray-800 hover:bg-gray-500 rounded-full cursor-pointer p-5 w-5 h-5 flex justify-center items-center opacity-50 hover:opacity-100">❮</Link>
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,44 +1,75 @@
 "use client";
 import ImageSlider from "./imageSlider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
-import data from "./data.json"
+import projectsData from "./projectsData.json";
+import componentsData from "./componentsData.json";
 
-const projects = data.filter((project) => {
+const projects = projectsData.filter((project) => {
   return !project.private;
-})
+});
+
+const components = componentsData.filter((components) => {
+  return !components.private;
+});
 
 type projectsType = {
+  type: String;
   projectId: Number;
   title: String;
   description: String;
-  createdOn: Date;
-  finishedOn: Date;
+  createdOn: String;
+  finishedOn: String;
   href: String;
   githubLink: String;
   screenshots: String[];
-  // comments: [
-  //   {
-  //     userId: 1,
-  //     userName: 'Pawan Kushwah',
-  //     message: "You portfolio is Awesome"
-  //   }
-  // ],
-  // githubDetails:
+  private: boolean;
+  screenshotPath: String;
+};
+
+type componentType = {
+  type: String;
+  componentId: Number;
+  title: String;
+  description: String;
+  createdOn: String;
+  finishedOn: String;
+  href: String;
+  githubLink: String;
+  screenshots: String[];
+  private:boolean;
+  screenshotPath: String;
 };
 
 export default function Home() {
-  const [cardJSX, setCardJSX] = useState(displayFilteredData(projects));
+  // using a state so that we can change the array from where we will filter out the data
+  // It is default to the projects
+  const [dataToSearchFrom, setDataToSearchFrom] = useState<(projectsType | componentType)[]>(projects);
+  const [itemNotFound, setItemNotFound] = useState(false);
 
-  function handleSearch(e: any) {
+  // It is being used for displaying the data in the form of cards when we search.
+  const [cardJSX, setCardJSX] = useState(displayFilteredData(dataToSearchFrom));
+
+  // useState for managing the selection of projects or components
+  function handleSearchTypeChange(e: React.ChangeEvent<HTMLSelectElement>): void {
+    console.log("changing type of Search", e.target.value);
+    e.target.value === "components" ? setDataToSearchFrom(components) : setDataToSearchFrom(projects);
+  }
+
+  useEffect(() => {
+    // when we change the searchType than dataToSearchFrom changes now we will change the card according to the current data
+    setCardJSX(displayFilteredData(dataToSearchFrom));
+  }, [dataToSearchFrom]);
+
+  function handleSearch(e: React.ChangeEvent<HTMLInputElement>): void {
     // Filter the object with the help of regex
-    const filteredArray = projects.filter((project) => {
+    const filteredArray = dataToSearchFrom.filter((data) => {
       const regex = new RegExp(e.target.value, "gi");
       return (
-        project.title.match(regex) ||
-        project.description.match(regex) ||
-        project.createdOn.toString().match(regex) ||
-        project.finishedOn?.toString().match(regex)
+        data.title.match(regex) ||
+        data.description.match(regex) ||
+        data.createdOn.toString().match(regex) ||
+        data.finishedOn?.toString().match(regex)
       );
     });
 
@@ -47,15 +78,16 @@ export default function Home() {
     setCardJSX(displayFilteredData(filteredArray));
   }
 
-  function displayFilteredData(filteredArray: projectsType[]) {
+  function displayFilteredData(filteredArray: (projectsType | componentType)[] ) {
     {
       return filteredArray.map((e) => {
+        const keyNo = 'projectId' in e ? e.projectId : e.componentId;
         return (
           <div
-            key={`${e.projectId}`}
+            key={keyNo.toString()}
             className="projectCard flex flex-col items-center text-center border-2 border-white rounded-lg overflow-hidden m-5 max-w-sm h-fit"
           >
-            <ImageSlider images={e.screenshots} />
+            <ImageSlider images={e.screenshots} imagePath={e.screenshotPath} />
             <Link
               href={`${e.href}`}
               className="p-5 text-xl md:text-2xl font-bold hover:text-blue-500"
@@ -73,12 +105,16 @@ export default function Home() {
   return (
     <>
       <div className="w-5 h-5 absolute top-0 left-0 bg-green-400 sm:bg-orange-400 md:bg-blue-400 lg:bg-red-400 xl:bg-black-400 2xl:bg-purple-400"></div>
-      <div className="flex justify-center align-center p-5 h-20">
+      <div className="flex flex-col gap-y-4 md:flex-row md:gap-0 justify-center align-center p-5 pt-10">
+        <select id="searchType" className="rounded-lg md:rounded-r-none p-2 px-5 text-xl bg-slate-600" onChange={handleSearchTypeChange}>
+          <option value="projects">Projects</option>
+          <option value="components">Components</option>
+        </select>
         <input
           type="search"
           onChange={handleSearch}
           id="searchBar"
-          className="bg-gray-700 rounded-lg text-white p-2 w-full text-xl md:w-[80%]"
+          className="bg-gray-700 rounded-lg md:rounded-l-none text-white p-2 px-5 w-full text-xl md:w-[40%]"
           placeholder="Search Projects..."
         />
       </div>
@@ -87,6 +123,13 @@ export default function Home() {
         className="min-h-screen pb-10 flex justify-center flex-wrap"
       >
         {cardJSX}
+        {
+          itemNotFound && (
+            <>
+              Not 
+            </>
+          )
+        }
       </div>
     </>
   );
